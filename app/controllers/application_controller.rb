d27030e9f6e4795e815416efba30e9335e69241e
tolaseadegbite <tolasekelvin@gmail.com>
2024-05-25 03:37:00 +0100

    accounts by last mesessage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
     before_action :turbo_frame_request_variant
+    before_action :set_current_account
 
     private
 
     def turbo_frame_request_variant
       request.variant = :turbo_frame if turbo_frame_request?
+    end
+
+    def set_current_account
+      Current.account = current_account
     end
 end
